1936829d8f6e4c3d3725ab6f1e90dfa87bf97f8c
NayanaLoivos <loivosdev@gmail.com>
2024-10-13 11:20:15 +0000

    Add Psicologo Model

---
+++ src/psiconexao/psiconexao/Models/Psicologo.cs
@@ -0,0 +1,58 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace psiconexao.Models
+{
+    [Table("Psicologos")]
+    public class Psicologo : Usuario {
+
+        [ForeignKey("UsuarioId")]
+        public int UsuarioId { get; set; }
+
+        [Required]
+        [StringLength(11)]
+        public string crp { get; set; }
+        
+        public string UrlFoto { get; set; }
+
+        public string Biografia { get; set; }
+
+        public float PrecoConsulta { get; set; }
+
+        //public virtual ICollection<Disponibilidade> Disponibilidades { get; set; };
+
+        [Display(Name ="Tipo de Especialidade")]
+        public TipoEspecialidade TEspecialidade { get; set; }
+
+        [Display(Name = "Tipo de Abordagem")]
+        public TipoAbordagem TAbordagem { get; set; }
+
+        public enum TipoEspecialidade
+        {
+            Clínica,
+            Escolar,
+            Saúde, 
+            Desenvolvimento,
+            Esporte, 
+            NeuroPsicologia,
+            Familiar,
+            Casal,
+            Transcultural,
+            Comportamental
+        }
+
+        public enum TipoAbordagem
+        {
+            CognitivoComportamental,
+            Psicanálise,
+            Humanista, 
+            Sistêmica,
+            Gestalt,
+            Narrativa,
+            Positiva,
+            Grupo,
+            Midfullness
+        }
+
+    }
+}
