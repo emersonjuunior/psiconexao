8f13f198eab288799d9f135572330567661b1146
Emerson Junior <emerson.c.junior.115@gmail.com>
2024-10-24 17:54:42 -0300

    implementado calendário na pag de criar disponibilidade

---
+++ src/psiconexao/psiconexao/wwwroot/js/disponibilidades.js
@@ -0,0 +1,25 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    const calendarEl = document.getElementById('calendar');
+    const today = new Date().toISOString().split('T')[0]; // Data atual no formato 'YYYY-MM-DD'
+
+    const calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'dayGridMonth', // Exibe o calendário mensalmente
+
+        // Impede marcação em datas anteriores à atual
+        validRange: {
+            start: today, // Apenas datas a partir de hoje são permitidas
+        },
+
+        selectable: true, // Permite selecionar uma data no calendário
+
+        dateClick: function (info) {
+            const selectedDate = info.dateStr.split("-").reverse().join("-");
+            console.log(selectedDate); // Exibe a data clicada
+        },
+
+    });
+
+    calendar.render(); // Renderiza o calendário
+});
+
+console.log("js funcionando");
