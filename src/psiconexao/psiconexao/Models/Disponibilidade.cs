faf0f0f647cb5613b20daf86985df92f3eca5add
Emerson Junior <emerson.c.junior.115@gmail.com>
2024-11-21 18:10:04 -0300

    alteração no display das propriedades no model disponibilidade

--- src/psiconexao/psiconexao/Models/Disponibilidade.cs
+++ src/psiconexao/psiconexao/Models/Disponibilidade.cs
@@ -11,15 +11,19 @@ namespace psiconexao.Models
         public int DisponibilidadeId { get; set; }
 
         [DataType(DataType.Date)]
+        [Display(Name = "Data Inicial")]
         public DateTime DataInicio { get; set; }
 
         [DataType(DataType.Date)]
+        [Display(Name = "Data Final")]
         public DateTime DataFim { get; set; }
 
         [DataType(DataType.Time)]
+        [Display(Name = "Horário Inicial")]
         public TimeSpan HoraInicio { get; set; }
 
         [DataType(DataType.Time)]
+        [Display(Name = "Horário Final")]
         public TimeSpan HoraFim { get; set; }
         public bool Estado { get; set; }
 
@@ -27,6 +31,7 @@ namespace psiconexao.Models
         public int PsicologoId { get; set; }
 
         [InverseProperty("Disponibilidades")]
+        [Display(Name = "Psicólogo")]
         public virtual Psicologo Psicologo { get; set; }
 
     }
